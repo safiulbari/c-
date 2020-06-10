a2a566b495517ad8b111cbcf7c1b724984dbe5fa
Safiul Bari <asmsafiulbari@gmail.com>
2020-06-10 22:08:43 +0600

    .exe file delted and add new problem solvation in practice folder

---
+++ 0.Practice/1.Introduction/1.Hello_World_program.cpp
@@ -0,0 +1,9 @@
+// My First c++ program 
+#include <iostream>		//header file for use cout
+using namespace std;	//cout in std
+
+
+int main(){
+	cout << "Hello World";	//printing string
+	return 0;
+}
