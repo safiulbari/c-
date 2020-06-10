94e337339bcc54e0a15c4543c98f18f9d9aa14c8
Safiul Bari <asmsafiulbari@gmail.com>
2020-06-10 21:37:17 +0600

    added no 1 folder

---
+++ C++ Introduction/dataTypes.cpp
@@ -0,0 +1,38 @@
+main(void){
+	int age = 30; //declaring integer type data size 2 or 4 bytes
+	
+	//data types in c++
+	//int - integer - 2 or 4 bytes
+	//float - floating point - 4 bytes
+	//double - double floating pint = 8 bytes
+	//char - characer - 1 byte
+	//wchar_t - wide character - 2 bytes
+	//bool - boolean - 1 bytes
+	//nul - Empty - 0 byte
+	
+	//integers example
+	int salary = 85000;
+	
+	//floating point example
+	float area = 64.74;
+	double volume = 134.64534;
+	double distance = 45E12; //E12 means *10^12
+	
+	//character example
+	char test = 'n';
+	
+	//wide character example
+	//It don't support in my compiler
+	
+	//boolean example
+	bool cond = false;
+	
+	
+	//type modiefier
+	//1. signed
+	//2. unsigned: only store positive number
+	//3. short
+	//4. long
+	
+	
+}
---
+++ C++ Introduction/variable.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+	
+	//In programming, a variable is a container (storage area) to hold data.
+	//each variable should have a unique name
+	
+	
+	//varible decalration
+	int age = 14;	//age is 14
+	age = 17;		//age is now 17
+	
+	//types of literals
+	//1. integers: decimal, octal, hexadecimal
+	//2. floating point literals: 0.0005, -.22E-5
+	//3. characters: 'a'
+	//4. escape sequences: \b, \f, \n, \t, \v, \\, \', \", \?, \0
+	//5. string literals: "This is a text which call string"
+	
+	
+	//constant declartion
+	const int LIGHT_SPEED = 2979458;
+
+	
+}
